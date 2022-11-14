1d271810323a551621403d3b37b72282862f5af1
quotcode <yshivam9920@gmail.com>
2022-11-14 23:36:22 +0530

    project started

---
+++ ecom-books/src/main/java/com/shivam/ecombooks/EcomBooksApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.ecombooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBooksApplication.class, args);
+	}
+
+}
---
+++ ecom-books/src/test/java/com/shivam/ecombooks/EcomBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.ecombooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
